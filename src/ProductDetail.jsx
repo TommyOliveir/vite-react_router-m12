e6d32a6bf9a29d0afa84c3a20daba7193ed5d60a
TommyOliveir <laridatommy@gmail.com>
2023-01-12 15:17:56 +0100

    ff

--- src/ProductDetail.jsx
+++ src/ProductDetail.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 function ProductDetail() {
     const {id} = useParams()
+    const [products, setProducts] = useState([])
+
+    useEffect(() => {
+      fetch('https://dummyjson.com/products')
+        .then(response => response.json())
+        // 4. Setting *dogImage* to the image url that we received from the response above
+        .then(data => setProducts(data.products))
+    }, [])
+    const thisproduct = products.find(prod => prod.id == id)
+   console.log(thisproduct)
   return (
     <div>
-      <h1>Using useParams</h1>
-      <h2>Product Detail {id} </h2>
+      <h3>Product Details</h3>
+      <h3>Using useParams</h3>
+      <p>Product Id: {id} </p>
+       <p>Product Name: <span className='product-name product-text-info'>{thisproduct.title}</span> </p>
+      <p>Description: <span className='product-description product-text-info'>{thisproduct.description}</span> </p>
+      <p>Product price: <span className='product-price product-text-info'>{thisproduct.price} Euro</span> </p> 
+    
+     
     </div>
   )
 }
--- src/Products.jsx
+++ src/Products.jsx
@@ -1,11 +1,35 @@
 import React from 'react'
-import { Link} from "react-router-dom"
+import { Link } from "react-router-dom"
+import { useEffect, useState } from 'react'
+import ProductDetail from './ProductDetail'
 
 function Products() {
+  const [products, setProducts] = useState([])
+
+  useEffect(() => {
+    fetch('https://dummyjson.com/products')
+      .then(response => response.json())
+      // 4. Setting *dogImage* to the image url that we received from the response above
+      .then(data => setProducts(data.products))
+  }, [])
+
+  const displayproducts = products.map(prod => (
+    <div key={prod.id}>
+      <Link to={`/products/${prod.id}`} ><h3>{prod.title}</h3></Link>
+
+      <hr />
+    </div>
+  ))
+
+
+  console.log(products)
+
+
   return (
     <div>
-      <h1>Products</h1>
-      <h4>Dynamic routes</h4>
+      <h1>Products are </h1>
+      <h4>(Dynamic routes)</h4>
+      {displayproducts}
       <Link to="/products/1">Product 1</Link><br></br>
       <Link to="/products/2">Product 2</Link>
     </div>
