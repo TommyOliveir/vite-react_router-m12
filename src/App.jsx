bffd813d6c5108721a0297715187f45f7825cefb
TommyOliveir <laridatommy@gmail.com>
2023-01-12 17:54:53 +0100

    s

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ import NoMatch from './Nomatch'
 import NestedRoute from './NestedRoute'
 import FeaturedProducts from './FeaturedProducts'
 import SearchParams from './SearchParams'
+import Profile from './Profile'
+import Login from './Login'
 //design active link - check css 
 function App() {
 
@@ -21,6 +23,7 @@ function App() {
         <NavLink to="/products">Products</NavLink>
         <NavLink to="/nestedroute">Nested route</NavLink>
         <NavLink to="/searchparams">Search params</NavLink>
+        <NavLink to="/profile">Profile</NavLink>
       </nav>
       <Routes>
         <Route path="/" element={<Home />} />
@@ -28,6 +31,8 @@ function App() {
         <Route path="/products/:id" element={<ProductDetail/>}/>
         <Route path="/Order" element={<Order/>}/>
         <Route path="/searchparams" element={<SearchParams/>}/>
+        <Route path="/profile" element={<Profile/>}/>
+        <Route path="/login" element={<Login/>}/>
         {/* nested route*/}
         <Route path='/nestedroute' element={<NestedRoute/>}>
           {/* index route */}
