c031a5e11a9b25217699c62338940c5cf9ecfadd
TommyOliveir <laridatommy@gmail.com>
2023-01-13 12:22:53 +0100

    finito

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import SearchParams from './SearchParams'
 import Profile from './Profile'
 import Login from './Login'
 import { useAuth } from './auth'
+import RequireAuth from './RequireAuth'
 //design active link - check css 
 function App() {
 
@@ -21,40 +22,46 @@ function App() {
 
   return (
     <div className="App">
-   
 
-        <h1>React-router Version 6 </h1>
-        <nav>
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="/products">Products</NavLink>
-          <NavLink to="/nestedroute">Nested route</NavLink>
-          <NavLink to="/searchparams">Search params</NavLink>
-          <NavLink to="/profile">Profile</NavLink>
+
+      <h1>React-router Version 6 </h1>
+      <nav>
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/products">Products</NavLink>
+        <NavLink to="/nestedroute">Nested route</NavLink>
+        <NavLink to="/searchparams">Search params</NavLink>
+        <NavLink to="/profile">Profile</NavLink>
         {
           !auth.user && (
             <NavLink to='/login'>Login</NavLink>
           )
         }
-        </nav>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:id" element={<ProductDetail />} />
-          <Route path="/Order" element={<Order />} />
-          <Route path="/searchparams" element={<SearchParams />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<Login />} />
-          {/* nested route*/}
-          <Route path='/nestedroute' element={<NestedRoute />}>
-            {/* index route */}
-            <Route index element={<FeaturedProducts />} />
-            <Route path='featured' element={<FeaturedProducts />} />
-            <Route path='new' element={<ProductNew />} />
-          </Route>
-          <Route path="*" element={<NoMatch />} />
-        </Routes>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<Products />} />
+        <Route path="/products/:id" element={<ProductDetail />} />
+        <Route path="/Order" element={<Order />} />
+        <Route path="/searchparams" element={<SearchParams />} />
 
-   
+        {/* protected route */}
+
+        <Route path="/profile" element={<RequireAuth><Profile /></RequireAuth>} />
+
+        <Route path="/login" element={<Login />} />
+
+        {/* nested route*/}
+        <Route path='/nestedroute' element={<NestedRoute />}>
+
+          {/* index route */}
+          <Route index element={<FeaturedProducts />} />
+          <Route path='featured' element={<FeaturedProducts />} />
+          <Route path='new' element={<ProductNew />} />
+        </Route>
+        <Route path="*" element={<NoMatch />} />
+      </Routes>
+
+
     </div>
   )
 }
--- src/login.jsx
+++ src/login.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, useLocation } from 'react-router-dom'
 import { useAuth } from './auth'
 
 function login() {
   const [user, setUser] = useState('')
   const auth = useAuth()
   const navigate = useNavigate()
+  const location = useLocation()
+
+  const redirectPath = location.state?.path || '/'
 
   const handleLogin = () => {
    auth.login(user)
-   navigate('/')
+   navigate(redirectPath, {replace: true})
   }
 
   return (
